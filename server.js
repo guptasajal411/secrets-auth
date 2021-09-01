b348214c77abdc991cf5fe1c040c0b5a28db0280
Sajal Gupta <guptasajal411@gmail.com>
2021-09-01 23:09:13 +0530

    Level 5 authentication with passport completed for registeration of new users

--- server.js
+++ server.js
@@ -6,19 +6,17 @@ const mongoose = require('mongoose');
 // const encrypt = require("mongoose-encryption");
 // This is a level 3 authentication method with md5 hashing
 // const md5 = require('md5');
-const bcrypt = require("bcrypt");
-const saltRounds = 10; //defining number of rounds for salting the password when hashing with bcrypt
+// level 4 authentication with bcrypt salting and hashing
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10; //defining number of rounds for salting the password when hashing with bcrypt
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 require("dotenv").config();
 
-mongoose.connect("mongodb+srv://" + process.env.usernameMongoDB + ":" + process.env.password + "@cluster0.xgjts.mongodb.net/secretsDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
 
 //  This is level 2 authentication configuration with mongoose-encryption
 // userSchema.plugin(encrypt, {
@@ -26,74 +24,75 @@ const userSchema = new mongoose.Schema({
 //     encryptedFields: ["password"]
 // });
 
-const User = new mongoose.model("User", userSchema);
-
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
+// Level 5 authentication
+app.use(session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize()); // initializes passport
+app.use(passport.session()); // starts up passport
+
+mongoose.connect("mongodb+srv://" + process.env.usernameMongoDB + ":" + process.env.password + "@cluster0.xgjts.mongodb.net/secretsDB", { useNewUrlParser: true, useUnifiedTopology: true })
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function (req, res) {
     res.render("home");
 });
 
 app.route("/login")
 
-.get(function(req, res) {
-    res.render("login");
-})
+    .get(function (req, res) {
+        res.render("login");
+    })
 
-.post(function(req, res) {
-    const email = req.body.email;
-    // This is a level 3 authentication method with md5 hashing
-    // const password = md5(req.body.password);
-    const password = req.body.password;
+    .post(function (req, res) {
 
-    User.findOne({ email: email}, function(err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                // This is a level 4 authentication method with bcrypt salting
-                bcrypt.compare(req.body.password, foundUser.password, function(error, result) {
-                    // result == true
-                    if (result == true) {
-                        res.render("secrets");
-                    } else {
-                        res.send("Wrong password. Try again");
-                    }
-                });
-            } else {
-                res.send("No user found.");
-            }
-        }
     });
-});
 
 app.route("/register")
 
-.get(function(req, res) {
-    res.render("register");
-})
+    .get(function (req, res) {
+        res.render("register");
+    })
 
-.post(function(req, res) {
-    bcrypt.hash(req.body.password, saltRounds, function(error, hash) { // This is a level 4 authentication method with bcrypt salting
-        const newUser = new User({
-            email: req.body.email,
-            // This is a level 3 authentication method with md5 hashing
-            // password: md5(req.body.password)
-            password: hash
-        });
-        newUser.save(function(err){
+    .post(function (req, res) {
+        User.register({ username: req.body.email }, req.body.password, function (err, user) {
             if (err) {
                 console.log(err);
-                res.send(err);
+                res.redirect("/register");
             } else {
-                res.render('secrets');
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
             }
         });
     });
-}); 
 
-app.listen(PORT, function() {
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()){
+        res.render("secrets"); 
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.listen(PORT, function () {
     console.log('listening on port: ' + PORT);
 });
